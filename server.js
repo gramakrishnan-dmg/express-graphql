938051a229de245e79b9192bb57d246b860d837a
Gayathri Ramakrishnan <gramakrishnan@divisionsinc.com>
2022-11-01 12:56:55 -0500

    fix: update graphqlHTTP for the method to see the correct error

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-const express= require("express");
+const express= require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}))
 app.listen(5000, () => console.log("Server is running"));
