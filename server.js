8c23c360160c83d834f2037e0c86e18d8ba534ed
Gayathri Ramakrishnan <gramakrishnan@divisionsinc.com>
2022-11-01 13:56:17 -0500

    feat: set up the base schema

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const expressGraphQL = require('express-graphql').graphqlHTTP;
 
 const app = express();
 
+const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require("graphql");
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "HelloWorld",
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+})
 app.use('/graphql', expressGraphQL({
+    schema,
     graphiql: true
 }))
 app.listen(5000, () => console.log("Server is running"));
